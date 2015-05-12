52d65a352106af4606a6ff8d54c06f94facab806
TimothePearce <timothe.pearce@gmail.com>
2015-05-12 16:37:55 +0200

    creation of the area game

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,36 @@
 var tableau = new Array();
-var tableauHTML = getElementById('areaGame');
 
 // Création du tableau
-function creerTableau() {
+function createTab() {
 	for (i = 0; i < 19; i++)
 	{
 		tableau[i] = [];
 
 		for (j = 0; j < 19; j++)
 		{
-			tableau[i][j] = 1;
+			tableau[i][j] = 0;
 		}
 	}
 }
 
-creerTableau();
+// Fonction qui écrit le tableau Game dans l'html
+function drawGame() {
+	var html = document.getElementById("plateau");
+	var draw = "";
+	
+	for (i = 1; i < 20; i++)
+	{
+		draw += "<tr>";
+
+		for (j = 1; j < 20; j++)
+		{
+			draw += "<td class = 'damier' id='"+ i + "_" + j + "'></td>";
+		}
+		draw += "</tr>";
+	}
+	html.innerHTML = draw;
+}
+
+createTab();
+drawGame();
 console.log(tableau);
