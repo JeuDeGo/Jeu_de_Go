60aea24a7d76f2863a516e147729ddb9631209ec
TimothePearce <timothe.pearce@gmail.com>
2015-05-12 17:38:06 +0200

    Checkerboard CSS done

--- scripts/app.js
+++ scripts/app.js
@@ -24,10 +24,24 @@ function drawGame() {
 
 		for (j = 1; j < 20; j++)
 		{
-			if ((i == 1) || (j == 1)) {
-				draw += "<td class = 'checkerboard' id='"+ i + "_" + j + "'></td>";
+			if ((i ==1) && (j == 1)) {
+				draw += "<td class = 'checkerboardCornerTopLeft' id='"+ i + "_" + j + "'></td>";
+			} else if ((i ==1) && (j == 19)) {
+				draw += "<td class = 'checkerboardCornerTopRight' id='"+ i + "_" + j + "'></td>";
+			} else if ((i ==19) && (j == 1)) {
+				draw += "<td class = 'checkerboardCornerBottomLeft' id='"+ i + "_" + j + "'></td>";
+			} else if ((i ==19) && (j == 19)) {
+				draw += "<td class = 'checkerboardCornerBottomRight' id='"+ i + "_" + j + "'></td>";
+			} else if (j == 1) {
+				draw += "<td class = 'checkerboardLeft' id='"+ i + "_" + j + "'></td>";
+			} else if (i == 1) {
+				draw += "<td class = 'checkerboardTop' id='"+ i + "_" + j + "'></td>";
+			} else if (j == 19) {
+				draw += "<td class = 'checkerboardRight' id='"+ i + "_" + j + "'></td>";
+			} else if (i == 19) {
+				draw += "<td class = 'checkerboardBottom' id='"+ i + "_" + j + "'></td>";
 			} else {
-			draw += "<td class = 'checkerboard' id='"+ i + "_" + j + "'></td>";
+			draw += "<td class = 'checkerboardCross' id='"+ i + "_" + j + "'></td>";
 			}
 		}
 		draw += "</tr>";
