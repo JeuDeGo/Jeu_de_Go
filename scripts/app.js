e5a8b6f9d6dedd09749a36db692f7e6f4d2255a4
TimothePearce <timothe.pearce@gmail.com>
2015-05-12 14:53:52 +0200

    resolve bugs with JS

--- scripts/app.js
+++ scripts/app.js
@@ -1,16 +1,21 @@
+var tableau = new Array();
+
 /**
 *Grille
 *
 **/
 
 function creerTableau() {
-	for (i = 0; i < X; i++)
+	for (i = 0; i < 19; i++)
 	{
 		tableau[i] = [];
 
-		for (j = 0; j < Y; j++)
+		for (j = 0; j < 19; j++)
 		{
 			tableau[i][j] = 1;
 		}
 	}
 }
+
+creerTableau();
+console.log(tableau);
