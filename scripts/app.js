42ec0ce255c6357ed7004a04eeb50a77b4231673
TimothePearce <timothe.pearce@gmail.com>
2015-05-14 00:16:14 +0200

    add on live function the incapacity to click on a cell who is a token

--- scripts/app.js
+++ scripts/app.js
@@ -67,29 +67,50 @@ drawGame();
 function live(eventType, elementId, callback) {
   document.addEventListener(eventType, function (event) {
       if (event.target.id === elementId) {
-        callback.call(event.target, event);
+        callback.call(event.target);
       }
   });
 }
 
-// Function who calcuate how many neighbour each token have, and wich kind (ennemi / friends)
+// Loop who add click on each intersection
+for (var i = 0; i < td.length; i++) {
+	var currentElement = td[i];
+	live('click', currentElement.id, function() {
+		var explode = this.id.split('_');
+		var isToken = this.className;
+		if (isToken != 'tokenWhite' || isToken != 'tokenBlack') { // Prevent click if the cell is a token
+			if (player == 'white') {
+				this.className = 'tokenWhite';
+				rules[rules.length] = new Token(explode[0], explode[1], 'white', rules.length);
+				player = 'black';
+			} else {
+				this.className = 'tokenBlack';
+				rules[rules.length] = new Token(explode[0], explode[1], 'black', rules.length);
+				player = 'white';
+			}
+			neighbour();
+		}
+	});
+}
+
+// Function who calcuate how many neighbour each token have, and wich kind (ennemi or friend)
 function neighbour() {
 	for (k = 0; k < rules.length; k++) {
 		rules[k].ennemi = 0;
 		rules[k].friend = 0;
 		for (l = 0; l < rules.length; l++) {
-			if ((k != l) && (rules[k].player != rules[l].player)) {													// ennemi :
+			if ((k != l) && (rules[k].player != rules[l].player)) { // ennemi :
 				if ((((rules[k].i) == (rules[l].i)) && ((rules[k].j) == (rules[l].j + 1))) || // right
-					(((rules[k].i) == (rules[l].i)) && ((rules[k].j) == (rules[l].j - 1))) ||		// left
-					(((rules[k].i) == (rules[l].i + 1)) && ((rules[k].j) == (rules[l].j))) ||		// bottom
-					(((rules[k].i) == (rules[l].i - 1)) && ((rules[k].j) == (rules[l].j))))	{		// top
+					(((rules[k].i) == (rules[l].i)) && ((rules[k].j) == (rules[l].j - 1))) || // left
+					(((rules[k].i) == (rules[l].i + 1)) && ((rules[k].j) == (rules[l].j))) ||	// bottom
+					(((rules[k].i) == (rules[l].i - 1)) && ((rules[k].j) == (rules[l].j))))	{	// top
 					rules[k].ennemi++;
 				}
-			} else if ((k != l) && (rules[k].player == rules[l].player)) {									// friend :
+			} else if ((k != l) && (rules[k].player == rules[l].player)) { // friend :
 				if ((((rules[k].i) == (rules[l].i)) && ((rules[k].j) == (rules[l].j + 1))) || // right
-					(((rules[k].i) == (rules[l].i)) && ((rules[k].j) == (rules[l].j - 1))) ||		// left
-					(((rules[k].i) == (rules[l].i + 1)) && ((rules[k].j) == (rules[l].j))) ||		// bottom
-					(((rules[k].i) == (rules[l].i - 1)) && ((rules[k].j) == (rules[l].j))))	{		// top
+					(((rules[k].i) == (rules[l].i)) && ((rules[k].j) == (rules[l].j - 1))) ||	// left
+					(((rules[k].i) == (rules[l].i + 1)) && ((rules[k].j) == (rules[l].j))) ||	// bottom
+					(((rules[k].i) == (rules[l].i - 1)) && ((rules[k].j) == (rules[l].j))))	{	// top
 					rules[k].friend++;
 				}
 			}
@@ -98,25 +119,7 @@ function neighbour() {
 	removeToken();
 }
 
-// Loop who add click on each intersection
-for (var i = 0; i < td.length; i++) {
-	var currentElement = td[i];
-	live('click', currentElement.id, function() {
-		var explode = this.id.split('_');
-		if (player == 'white') {
-			this.className = 'tokenWhite';
-			rules[rules.length] = new Token(explode[0], explode[1], 'white', rules.length);
-			player = 'black';
-		} else {
-			this.className = 'tokenBlack';
-			rules[rules.length] = new Token(explode[0], explode[1], 'black', rules.length);
-			player = 'white';
-		}
-		neighbour();
-	});
-}
-
-// Function who remove the token circled by 4 ennemis
+// Function who remove a token circled by 4 ennemis
 function removeToken() {
 	for (i = 0; i < rules.length; i++) {
 		if ((rules[i].friend == 0) && (rules[i].ennemi == 4)) {
