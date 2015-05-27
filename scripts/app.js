e82016e3c995df217862f26448762a1ed5b62210
TimothePearce <timothe.pearce@gmail.com>
2015-05-27 15:34:26 +0200

    solve function bug

--- scripts/app.js
+++ scripts/app.js
@@ -7,28 +7,28 @@ var Game = {
 	},
 	token : new Array(),
 	constructor : {
-		CreateToken : function(i, j, tabPosition) {
+		CreateToken : function(i, j) {
 			this.i = parseInt(i);
 			this.j = parseInt(j);
-			this.tabPosition = parseInt(tabPosition);
 			this.player = Game.data.player;
 			this.initialLiberty = Game.otherFunction.libertyNumber(i, j);
 			this.liberty = this.initialLiberty;
 			this.friendTabPosition;
 			this.group = undefined;
       this.neighbour = 0;
+      this.tabPosition = i + '_' + j;
       this.friendTabPosition = new Array();
 		}
 	},
 	otherFunction : {
 		libertyNumber : function (i, j) {
-			if (i == 1 && j == 1 || i == 1 && j == 19 || i == 19 && j == 1 || i == 19 && j == 19) {
+			if (i == 0 && j == 0 || i == 0 && j == 18 || i == 18 && j == 0 || i == 18 && j == 18) {
 
 				return 2;
-			} else if (i == 1 && j != 1 || i == 1 && j != 19 || i == 19 && j != 1 || i == 19 && j != 19) {
+			} else if (i == 0 && j != 0 || i == 0 && j != 18 || i == 18 && j != 0 || i == 18 && j != 18) {
 
 				return 3;
-			} else if (j == 1 && i != 1 || j == 1 && i != 19 || j == 19 && i != 1 || j == 19 && i != 19) {
+			} else if (j == 0 && i != 0 || j == 0 && i != 18 || j == 18 && i != 0 || j == 18 && i != 18) {
 
 				return 3;
 			} else {
@@ -44,25 +44,25 @@ function drawGame() {
 	var html = document.getElementById("board");
 	var draw = "";
 	
-	for (i = 1; i < 20; i++) {
+	for (i = 0; i < 19; i++) {
 		draw += "<tr>";
-		for (j = 1; j < 20; j++) {
+		for (j = 0; j < 19; j++) {
 			draw += "<td class = "
-			if ((i ==1) && (j == 1)) {
+			if ((i == 0) && (j == 0)) {
 				draw += "'checkerboardCornerTopLeft'";
-			} else if ((i ==1) && (j == 19)) {
+			} else if ((i == 0) && (j == 18)) {
 				draw += "'checkerboardCornerTopRight'";
-			} else if ((i ==19) && (j == 1)) {
+			} else if ((i == 18) && (j == 0)) {
 				draw += "'checkerboardCornerBottomLeft'";
-			} else if ((i ==19) && (j == 19)) {
+			} else if ((i == 18) && (j == 18)) {
 				draw += "'checkerboardCornerBottomRight'";
-			} else if (j == 1) {
+			} else if (j == 0) {
 				draw += "'checkerboardLeft'";
-			} else if (i == 1) {
+			} else if (i == 0) {
 				draw += "'checkerboardTop'";
-			} else if (j == 19) {
+			} else if (j == 18) {
 				draw += "'checkerboardRight'";
-			} else if (i == 19) {
+			} else if (i == 18) {
 				draw += "'checkerboardBottom'";
 			} else {
 				draw += "'checkerboardCross'";
@@ -75,103 +75,17 @@ function drawGame() {
 }
 drawGame();
 
-// Function who calcuate how many neighbour each token have, and wich kind (ennemi or friend)
-function neighbourhood() {
-  for (j = 0; j < Game.token.length; j++) Game.token[j].group = undefined; // reset group
-  Game.data.group = [];
-
-  for (k = 0; k < Game.token.length; k++) {
-    var A = Game.token[k];
-
-    A.liberty = A.initialLiberty;
-    A.friendTabPosition = [];
-
-    for (l = 0; l < Game.token.length; l++) {
-      var B = Game.token[l];
-
-      if ((k != l && A.player !== B.player) && 
-      ((A.i == B.i && A.j == B.j + 1) || // right
-      (A.i == B.i && A.j == B.j - 1) || // left
-      (A.i == B.i + 1 && A.j == B.j) || // bottom
-      (A.i == B.i - 1 && A.j == B.j)))  { // top
-        A.liberty--; // Remove liberty
-      } else if ((k != l && A.player == B.player) &&  // manage group :
-      ((A.i == B.i && A.j == B.j + 1) || // right
-      (A.i == B.i && A.j == B.j - 1) || // left
-      (A.i == B.i + 1 && A.j == B.j) || // bottom
-      (A.i == B.i - 1 && A.j == B.j)))  { // top
-        A.liberty--; // Remove liberty
-        group(A, B);
-        A.friendTabPosition.push(B.tabPosition);
-        B.friendTabPosition.push(A.tabPosition);
-      }
-    }
+// Function who calculate who is the neighbour of the target token
+function neighbourhood(i, j, tokenColor) {
+  if (i != 0 && Game.token[(i - 1)][j] != undefined) { // neighboor on top
+    console.log('neighboor on top');
+  }
+  console.log(i + 1 + '_' + j);
+  if (i != 19 && Game.token[(i + 1)][j] != undefined) { // neighboor on bottom
+    console.log('neighboor on bottom');
   }
 }
 
-function group(A, B) {
-  if (A.group == undefined && B.group == undefined) {
-  console.log('group'), // create new group
-    A.group = Game.data.group.length;
-    B.group = A.group;
-    Game.data.group[A.group] = new Array();
-    Game.data.group[A.group].push(A.tabPosition);
-  } else if (A.group != undefined && B.group != undefined && A.group != B.group) { // merge group A with group B
-    mergeGroup(A.group, B.group);
-  } else if (A.group != undefined && B.group != undefined && A.group == B.group) {
-    Game.data.group[A.group].push(A.tabPosition);
-  } else if (A.group == undefined && B.group != undefined) { // add group to A.group
-    A.group = B.group;
-    Game.data.group[A.group].push(A.tabPosition);
-  } else if (A.group != undefined && B.group == undefined) { // add group to B.group
-    B.group = A.group;
-  }
-}
-
-function mergeGroup(A, B) {
-  if (A < B) {
-    for (m = 0; m < Game.data.group[B].length; m++) {
-      Game.data.group[A].push(Game.data.group[B][m]);
-      //Game.token[Game.data.group[B][m]].group = A;
-      for (j = Game.data.group[B]; j < Game.data.group.length; j++) Game.data.group[j].tabPosition = j;
-    }
-    // Game.data.group.splice(B, 1);
-    Game.data.group[B] = 0;
-  } else {
-    for (m = 0; m < Game.data.group[A].length; m++) {
-      Game.data.group[B].push(Game.data.group[A][m]);
-      //Game.token[Game.data.group[B][m]].group = A;
-    }
-  }
-}
-
-// Function who remove a token circled by 4 ennemis
-function removeToken() {
-  for (i = 0; i < Game.token.length; i++) {
-    if (Game.token[i].group == undefined && Game.token[i].liberty == 0) { // Token without group
-      var removeIt = document.getElementById(Game.token[i].i + '_' + Game.token[i].j);
-      removeIt.className = 'checkerboardCross'; // NEED TO ADD CLASS IN FONCTION OF I AND J
-      Game.token.splice(Game.token[i].tabPosition, 1); // Delete token in Game.token tab
-
-      for (j = 0; j < Game.token.length; j++) Game.token[j].tabPosition = j; // actualise group position of all token
-    } else if ( Game.token[i].group != undefined && Game.token[i].liberty == 0) {
-      var check = Game.data.group[Game.token[i].group].length;
-
-      for (j = 0; j < Game.data.group[Game.token[i].group].length; j++) {
-        if (Game.token[Game.data.group[Game.token[i].group][j]].liberty == 0) check--;
-      }
-
-      if (check == 0) {
-        for (j = 0; j < Game.data.group[Game.token[i].group].length; j++) {
-          var removeIt = document.getElementById(Game.token[Game.data.group[Game.token[i].group][j]].i + '_' + Game.token[Game.data.group[Game.token[i].group][j]].j);
-          removeIt.className = 'checkerboardCross'; // NEED TO ADD CLASS IN FONCTION OF I AND J
-        }
-        // NEED TO ADD REMOVING OF Game.token[k] !!!
-        // NEED TO ADD REMOVING OF Game.data.group[Game.token[k].group] !!!
-      }
-    }
-  }
-}
 
 // Function who add an EventListener on an event
 function live(eventType, elementId, callback) {
@@ -191,14 +105,14 @@ for (var i = 0; i < Game.data.td.length; i++) {
 		if (isToken != 'tokenWhite' && isToken != 'tokenBlack') { // Prevent click if the cell is a token
 			if (Game.data.player == 'white') {
 				this.className = 'tokenWhite';
-				Game.token.push(new Game.constructor.CreateToken(explode[0], explode[1], Game.token.length));
+				Game.token[explode[0]][explode[1]] = new Game.constructor.CreateToken(explode[0], explode[1]);
+        neighbourhood(explode[0], explode[1], Game.token[explode[0]][explode[1]].player);
 			} else {
 				this.className = 'tokenBlack';
-				Game.token.push(new Game.constructor.CreateToken(explode[0], explode[1], Game.token.length));
+        Game.token[explode[0]][explode[1]] = new Game.constructor.CreateToken(explode[0], explode[1]);
+        neighbourhood(explode[0], explode[1], Game.token[explode[0]][explode[1]].player);
 			}
       Game.data.player = ((Game.data.player == 'white') ? 'black' : 'white');
-			neighbourhood();
-			removeToken();
 		}
 	});
 }
@@ -208,6 +122,13 @@ for (i = 0; i < 19; i++) {
   Game.data.groupPosition[i] = [];
   for (j = 0; j < 19; j++) {
     Game.data.groupPosition[i][j] = undefined;
+  }
+}
+
+for (i = 0; i < 19; i++) {
+  Game.token[i] = [];
+  for (j = 0; j < 19; j++) {
+    Game.token[i][j] = undefined;
   }
 }
 
