4ed0d980a89854a64712b042f62ffa105b16369b
JC <jean-christophe.pellat@ovh.fr>
2015-06-26 00:53:54 +0200

    chat integration (without room messages)

--- app.js
+++ app.js
@@ -178,7 +178,18 @@ io.on( "connection", function(socket, nickname)
 
   // envoyer nickname quand on est sur la zone de jeu.
 
+      // Dès qu'on nous donne un pseudo, on le stocke en variable de session et on informe les autres personnes
+      socket.on('nouveau_client', function(pseudo) {
+          pseudo = ent.encode(pseudo);
+          socket.pseudo = pseudo;
+          socket.broadcast.emit('nouveau_client', pseudo);
+      });
 
+      // Dès qu'on reçoit un message, on récupère le pseudo de son auteur et on le transmet aux autres personnes
+      socket.on('message', function (message) {
+          message = ent.encode(message);
+          socket.broadcast.emit('message', {pseudo: socket.pseudo, message: message});
+      }); 
 
 });
 
